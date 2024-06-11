5ce172da86c60bd0d951cc9f87c3e4095e583267
Ray-Cheng-11 <ray20150603@gmail.com>
2024-06-11 17:52:57 +0800

    mlp

--- mlp.py
+++ mlp.py
@@ -13,64 +13,30 @@ class MLModel(BaseModel, ABC):
         super(MLModel, self).__init__(model, trained)
 
     def save(self, path: str, name: str) -> None:
-        """
-        保存模型
-
-        Args:
-            path (str): 模型路径
-            name (str): 模型文件名
-        """
+        
         save_path = os.path.abspath(os.path.join(path, name + '.m'))
         pickle.dump(self.model, open(save_path, "wb"))
 
     @classmethod
     def load(cls, path: str, name: str):
-        """
-        加载模型
 
-        Args:
-            path (str): 模型路径
-            name (str): 模型文件名
-        """
         model_path = os.path.abspath(os.path.join(path, name + '.m'))
         model = joblib.load(model_path)
         return cls(model, True)
 
     def train(self, x_train: np.ndarray, y_train: np.ndarray) -> None:
-        """
-        训练模型
 
-        Args:
-            x_train (np.ndarray): 训练集样本
-            y_train (np.ndarray): 训练集标签
-        """
         self.model.fit(x_train, y_train)
         self.trained = True
 
     def predict(self, samples: np.ndarray) -> np.ndarray:
-        """
-        预测音频的情感
-
-        Args:
-            samples (np.ndarray): 需要识别的音频特征
-
-        Returns:
-            results (np.ndarray): 识别结果
-        """
+        
         if not self.trained:
             raise RuntimeError('There is no trained model.')
         return self.model.predict(samples)
 
     def predict_proba(self, samples: np.ndarray) -> np.ndarray:
-        """
-        预测音频的情感的置信概率
-
-        Args:
-            samples (np.ndarray): 需要识别的音频特征
-
-        Returns:
-            results (np.ndarray): 每种情感的概率
-        """
+        
         if not self.trained:
             raise RuntimeError('There is no trained model.')
 
